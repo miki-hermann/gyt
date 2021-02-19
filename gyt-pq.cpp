f716a0cd88b88a33ccedc3710ff3f6286a075828
Miki Hermann <76700074+miki-hermann@users.noreply.github.com>
2021-02-19 17:29:24 +0100

    Add files via upload

--- gyt-pq.cpp
+++ gyt-pq.cpp
@@ -17,7 +17,7 @@ typedef pair<val_tuple, unsigned long long> val_res;
 struct cmp_vr {
   bool operator()(const val_res &vr1, const val_res &vr2)
   {
-    return vr1.second > vr2.second;
+    return vr1.second >= vr2.second;
   }
 };
 
